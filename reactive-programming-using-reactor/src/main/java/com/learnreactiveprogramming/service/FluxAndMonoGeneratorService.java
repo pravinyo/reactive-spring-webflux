60c596faa6c4b8f5bf1ab527a7f35d6f833dfa2f
Pravin Tripathi <pravinyo12@gmail.com>
2021-12-16 23:46:58 +0530

    simple flux example

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,18 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("Pravin", "Ram"));
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
+        service.namesFlux()
+                .subscribe(name -> System.out.println("Name is "+ name));
+    }
 }
