dc7050c146df4533baf33f4c4cfb67e33f62f06f
Pravin Tripathi <pravinyo12@gmail.com>
2021-12-17 17:10:51 +0530

    test stream using step verifier

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,21 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService service = new FluxAndMonoGeneratorService();
+
+    @Test
+    void test_namesFlux() {
+        var namesFlux = service.namesFlux();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("Pravin", "Ram")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
 
 }
