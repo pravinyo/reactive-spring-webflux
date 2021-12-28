5af272a0889e236980cd14f8ef82f7ccac5880b5
Pravin Tripathi <pravinyo12@gmail.com>
2021-12-28 23:37:08 +0530

    fixed issue for component missing

--- movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
@@ -2,10 +2,12 @@ package com.reactivespring.client;
 
 import com.reactivespring.domain.Review;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 
+@Component
 public class ReviewsRestClient {
     private final WebClient webClient;
 
--- movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
@@ -1,5 +1,7 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.client.MoviesInfoRestClient;
+import com.reactivespring.client.ReviewsRestClient;
 import com.reactivespring.domain.Movie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,21 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/v1/movies")
 public class MoviesController {
 
+    private final MoviesInfoRestClient moviesInfoRestClient;
+    private final ReviewsRestClient reviewsRestClient;
+
+    public MoviesController(MoviesInfoRestClient moviesInfoRestClient, ReviewsRestClient reviewsRestClient) {
+        this.moviesInfoRestClient = moviesInfoRestClient;
+        this.reviewsRestClient = reviewsRestClient;
+    }
+
     @GetMapping("/{id}")
     public Mono<Movie> retrieveMovieById(@PathVariable("id") String movieId) {
-        return null;
+        return moviesInfoRestClient.retrieveMovieInfo(movieId)
+                .flatMap(movieInfo -> {
+                    var reviewsListMono = reviewsRestClient.retrieveMovieReviews(movieId)
+                            .collectList();
+                    return reviewsListMono.map(reviews -> new Movie(movieInfo, reviews));
+                });
     }
 }
