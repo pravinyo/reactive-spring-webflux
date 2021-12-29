fc9710b9808995d2e3af1422305f46a8924931d6
Pravin Tripathi <pravinyo12@gmail.com>
2021-12-29 23:42:31 +0530

    exception handling for rest failure

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -1,15 +1,19 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class MoviesInfoRestClient {
 
-    private WebClient webClient;
+    private final WebClient webClient;
 
     @Value("${restClient.moviesInfoUrl}")
     private String moviesInfoUrl;
@@ -24,6 +28,19 @@ public class MoviesInfoRestClient {
                 .get()
                 .uri(url, movieId)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                    log.info("Status code is : {}", clientResponse.statusCode().value());
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)){
+                        return Mono.error(new MoviesInfoClientException(
+                                "There is no movie present with Id:"+movieId,
+                                clientResponse.statusCode().value()
+                        ));
+                    }
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(clientMessage -> Mono.error(new MoviesInfoClientException(
+                                    clientMessage, clientResponse.statusCode().value()
+                            )));
+                })
                 .bodyToMono(MovieInfo.class)
                 .log();
     }
